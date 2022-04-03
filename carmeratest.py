7f8960e0cced5c0bd43c223b4da111e7cdd59e48
PJS <cyc05326@nate.com>
2022-04-03 12:10:11 +0900

    커밋 테스트

--- carmeratest.py
+++ carmeratest.py
@@ -3,11 +3,11 @@
 import cv2
 
 capture = cv2.VideoCapture(0)#카메라 장치번호
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) #가로
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 800) #세로
 
 while cv2.waitKey(1) != ord('q'):#q버튼 누를때 종료
-    ret, frame = capture.read()#ret=카메라 상태 저장 작동시 true그렇지 않으면 false저장
+    ret, frame = capture.read()#ret=카메라 상태 저장 작동시true 그렇지 않으면 false저장
     cv2.imshow("VideoFrame", frame)
 
 capture.release()
