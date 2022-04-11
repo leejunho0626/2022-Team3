2f1ae780826e13543aaa329068e8a1f1b19b9f19
leejunho0626 <cse05214@gmail.com>
2022-04-11 18:08:19 +0900

    파이썬-firebase 연동

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Firebase database 인증 및 앱 초기화
+cred = credentials.Certificate('key.json')
+firebase_admin.initialize_app(cred,{
+    'databaseURL' : 'https://teamproject-642cf-default-rtdb.firebaseio.com/'
+    #'databaseURL' : '데이터 베이스 url'
+})
+
+#db 위치 지정, 기본 가장 상단을 가르킴
+ref = db.reference('불량검사 결과/제품1') #경로가 없으면 생성한다.
+ref.update({'가로' : '10'})
+ref.update({'세로' : '20'})
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
