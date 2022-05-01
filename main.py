317ab4835a9c8bfb6e3f730cded32578a985c9c4
leejunho <cse05214@gmail.com>
2022-05-01 17:15:59 +0900

    불량검사 후 DB에 값 전송(테스트)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import uuid
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import auth
+from firebase_admin import firestore
 import random
 from datetime import datetime
 from tkinter import *
@@ -11,10 +14,11 @@ import tkinter.messagebox
 
 
 cred = credentials.Certificate('key.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://teamproject-642cf-default-rtdb.firebaseio.com/'
-    # 'databaseURL' : '데이터 베이스 url'
-})
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
 
 """user = auth.get_user(None)"""
 
@@ -28,7 +32,9 @@ window.resizable(False, False)
 # 사용자 id와 password를 저장하는 변수 생성
 user_id, userID, password = StringVar(), StringVar(), StringVar()
 
-user_round, user_area = StringVar(), StringVar()
+user_round, user_area, User_ID= StringVar(), StringVar(), StringVar()
+
+
 
 
 def inputValue():
@@ -42,39 +48,106 @@ def inputValue():
         window2.title('불량제품 검사')
         window2.geometry('600x600+500+100')
         window2.resizable(False, False)
-        Label(window2, text="정상 제품 기준치 입력").grid(row=0, column=0, padx=10, pady=10)
-        Label(window2, text="현재 접속한 ID : " + user).grid(row=0, column=1, padx=10, pady=10)
-        Label(window2, text="둘레를 입력하세요.").grid(row=1, column=0, padx=10, pady=10)
-        Label(window2, text="넓이를 입력하세요.").grid(row=2, column=0, padx=10, pady=10)
-        Entry(window2, textvariable=user_round).grid(row=1, column=1, padx=10, pady=10)
-        Entry(window2, textvariable=user_area).grid(row=2, column=1, padx=10, pady=10)
-        Button(window2, text="등록", command=lambda: messagePrint(user, window2)).grid(row=3, column=1, padx=10, pady=10)
+
+        label_main = Label(window2, text="정상 제품 기준치 입력")
+        label_main.pack()
+
+        label_ID = Label(window2)
+        label_ID['text'] = '현재 접속한 ID : ' + user
+        label_ID.pack()
+
+        label_round = Label(window2, text="둘레를 입력하세요.")
+        label_round.pack()
+
+        ent_round = Entry(window2)
+        ent_round.pack()
+
+        label_area = Label(window2, text="넓이를 입력하세요.")
+        label_area.pack()
+
+        ent_area = Entry(window2)
+        ent_area.pack()
+
+        btn_register = Button(window2, text="등록", command=lambda : messagePrint(user, window2, ent_round.get(), ent_area.get()))
+        btn_register.pack()
+
         window2.mainloop()
+
     except:
         print("오류")
 
 
 ##메시지 박스
-def messagePrint(id, w):
-    print('아이디:', id)
+def messagePrint(id, w, round, area):
+
+    print('아이디:', id, round)
     msg = tkinter.messagebox.askquestion('등록 완료', '제품 검사를 진행하시겠습니까?')
     if msg == 'yes':
         print('진행')
+
+
+
         ##검사 진행
         w.destroy()
-        checkStart()
+        checkStart(id,round, area)
 
 
     else:
         tkinter.messagebox.showinfo('취소', '취소되었습니다.')
 
-
-def checkStart():
+## 제품 측정 시작
+def checkStart(id, round, area):
     window3 = Tk()
     window3.title('불량제품 검사')
     window3.geometry('600x600+500+100')
     window3.resizable(False, False)
-    Label(window3, text="검사 진행 1단계 제품 측정").grid(row=0, column=0, padx=10, pady=10)
+    Label(window3, text="검사 진행 제품 측정").grid(row=0, column=0, padx=10, pady=10)
+
+    ##임의값 생성(테스트용)
+
+    result_round =[]
+    result_area = []
+
+    ##둘레
+    for i in range(1,6):
+        x = random.randrange(10, 20)
+        result_round.append(x)
+        print(result_round)
+    ##넓이
+    for i in range(1, 6):
+        x = random.randrange(10, 20)
+        result_area.append(x)
+        print( result_area)
+
+    number = 0
+    for test in result_round:
+        number = number+1 ##순서
+        ##정상일 때
+        if test == int(round): ##정상 조건
+            print("%d번 정상" % number)
+            ## 데이터 DB에 전송
+            now = datetime.now() ##시간
+            doc_ref = db.collection(id).document(str(number))
+            doc_ref.set({
+                u'normal_round': result_round[number-1], ##배열[number]하면 배열[1]부터 시작하기 때문에 배열[0]부터 하기위해서 -1을 함.
+                u'normal_area': result_area[number-1],
+                u'User': id,
+                u'result': '정상',
+                u'time': str(now)
+            })
+        ##불량일 때
+        else:
+            print("%d번 불량" % number)
+            ## 데이터 DB에 전송
+            now = datetime.now()
+            doc_ref = db.collection(id).document(str(number))
+            doc_ref.set({
+                u'normal_round': result_round[number - 1],
+                u'normal_area': result_area[number - 1],
+                u'User': id,
+                u'result': '불량',
+                u'time': str(now)
+            })
 
 
 label = Label(window)
@@ -120,58 +193,8 @@ def changeFrame():
     except:
         print("오류")
 
-
 btnLogin = Button(window, text="로그인", command=inputValue)
 btnLogin.pack()
-
-"""
-results = []
-results2 = []
-##라벨
-label = Label(window)
-label['text']='둘레를 입력하세요.'
-label.pack()
-
-label3 = Label(window)
-def w(n):
-    label3.config(text = "입력한 둘레 길이입니다. > "+ ent.get()+"cm")
-    print('세로:', ent.get())
-    results.append(ent.get())
-##입력창
-ent=Entry(window)
-ent.bind("<Return>", w)
-ent.pack()
-label3.pack()
-
-label2 = Label(window)
-label2['text']=' 면적을 입력하세요.'
-label2.pack()
-
-label4 = Label(window)
-def h(n):
-    label4.config(text = "입력한 면적 크기입니다. > "+ ent2.get()+"cm")
-    print('세로:', ent2.get())
-    results2.append(ent2.get())
-    print(results, results2)
-
-
-    a=4
-    abc = '제품' + str(a)
-    now = datetime.now()
-    # db 위치 지정, 기본 가장 상단을 가르킴
-    ref = db.reference(abc)  # 경로가 없으면 생성한다.
-    ref.update({'둘레': ent.get()})
-    ref.update({'면적': ent2.get()})
-    ref.update({'시간': str(now)})
-    ref.update({'ID': "test"})
-
-
-##입력창
-ent2=Entry(window)
-ent2.bind("<Return>", h)
-ent2.pack()
-label4.pack()
-"""
 
 window.mainloop()
 
